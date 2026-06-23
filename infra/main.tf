6dd75acbb09717164417417372edf47cfd42249f
DevOps Student <devops@example.com>
2026-06-23 23:44:10 +0200

    fix terraform formatting

--- infra/main.tf
+++ infra/main.tf
@@ -7,24 +7,21 @@ terraform {
   }
 }
 
-# Windows avec Docker Desktop
-provider "docker" {
-  host = "npipe:////./pipe/docker_engine"
+provider "docker" {}
+
+variable "image_tag" {
+  type = string
 }
 
-# Reseau Docker partage Jenkins / SonarQube / SentimentAI
-# Ce reseau existe deja depuis le TP2/TP3 -- voir Partie 3.1
-resource "docker_network" "cicd" {
-  name = "cicd-network"
+resource "docker_image" "sentiment_ai" {
+  name = "sentiment-ai:${var.image_tag}"
+
+  build {
+    context    = "../"
+    dockerfile = "../Dockerfile"
+  }
 }
 
-# Image Docker SentimentAI -- image LOCALE buildee par Jenkins
-resource "docker_image" "sentiment" {
-  name         = "sentiment-ai:${var.image_tag}"
-  keep_locally = true
-}
-
-# Conteneur staging
 resource "docker_container" "sentiment_staging" {
   name  = "sentiment-staging"
   image = docker_image.sentiment_ai.name
